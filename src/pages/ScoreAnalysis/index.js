a21cfa7535dadb547a1c58db4067f202457dc58d
Ciel.S <xiaoyunshih@gmail.com>
2019-09-30 16:35:21 +1000

    score anlysis index page

--- src/pages/ScoreAnalysis/index.js
+++ src/pages/ScoreAnalysis/index.js
@@ -2,23 +2,28 @@
 // 成绩单分析首页
 
 Page({
-  data: {},
-  // get_result: function(e){
-  //   wx.navigateTo({
-  //     url:'../chengjidan/fenxi'
-  //   })
-  // },
+  data: {
+    score: [
+      { name: "target", value: "Target Score", value_cn: "目标" },
+      { name: "overall", value: "Overall Score", value_cn: "总分" },
+      { name: "listening", value: "Listening", value_cn: "听力" },
+      { name: "speaking", value: "Speaking", value_cn: "口语" },
+      { name: "writing", value: "Writing", value_cn: "写作" },
+      { name: "reading", value: "Reading", value_cn: "阅读" },
+      { name: "grammar", value: "Grammar", value_cn: "语法" },
+      { name: "fluency", value: "Olal Fluency", value_cn: "流利" },
+      { name: "pronunciation", value: "Pronunciation", value_cn: "发音" },
+      { name: "spelling", value: "Spelling", value_cn: "拼写" },
+      { name: "vocabulary", value: "Vocabulary", value_cn: "单词" },
+      { name: "discourse", value: "Written Discourse", value_cn: "表达" }
+    ]
+  },
 
   //确认按钮把数据上传后台
   upload_score: function(e) {
-    var that = this;
     var formData = e.detail.value;
-    // var target = e.detail.value.target; //目标分数
-    // var listening = e.detail.value.listening; //听力成绩
-    // var speaking = e.detail.value.speaking; //口语成绩
-    // var writing = e.detail.value.writing; //写作成绩
-    // var reading = e.detail.value.reading; //阅读成绩
 
+    // 把成绩数据存储在本地缓存中，后期可以改成存在服务器数据库中，把往期的考试成绩也保存下来，加入考试日期信息
     try {
       wx.setStorage({
         key: "score",
@@ -27,24 +32,22 @@ Page({
           listening: formData.listening,
           speaking: formData.speaking,
           writing: formData.writing,
-          reading: formData.reading
+          reading: formData.reading,
+          grammar: formData.grammar,
+          fluency: formData.fluency,
+          pronunciation: formData.pronunciation,
+          spelling: formData.spelling,
+          vocabulary: formData.vocabulary,
+          discourse: formData.discourse
         }
       });
     } catch (e) {
       console.log("setstoragesync fail");
+      // 这里要改进以下，存储失败的话就跳出提示框重新尝试之类的
     }
 
     wx.navigateTo({
       url: "../ScoreAnalysis/result"
     });
-
-    if (listening > 65) {
-      KEYI;
-    }
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {}
+  }
 });
